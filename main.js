323012b48d6f453b5b820303d2b14c2a7ed2724f
vinay13000 <74045128+vinay13000@users.noreply.github.com>
2021-10-24 20:36:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function addUser() {
       localStorage.setItem("player1_name", player1_name);
       localStorage.setItem("player2_name", player2_name);
   
-      window.location = "game_page.html";
+      window.location = "game.html";
   }
   
   
